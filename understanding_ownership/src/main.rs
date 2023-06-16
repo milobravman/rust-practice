be1e3c77a80e31a83ccfe7358a52bf6a669cab59
milolynch <milobravman20@gmail.com>
2023-06-16 18:05:23 -0400

    comments

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -4,4 +4,9 @@ fn main() {
     // after 'let second ...' any reference to first does not work because first no longer owns the string  
     let second = first; 
     println!("{second}");
+    // println! ("{first}") would cause an error because we can no longer reference this variable
+
+    // need to add example on references and borrowing
+
+    // need add example on ownership errors
 }
