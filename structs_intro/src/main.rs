437f154edee006151645671e239d35305373778c
milolynch <milobravman20@gmail.com>
2023-06-20 14:47:14 -0400

    first version of area calculation. also practicing trying to write code without looking or using a guide

--- structs_intro/src/main.rs
+++ structs_intro/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+
+    // let width = 30;
+    // let height = 50;
+
+    // println!(
+    //     "the area of the rectangle is {} pixels", area(width, height)
+    // );
+
+    // fn area (width: u32, height: u32) -> u32{
+    //     width * height
+    // }
+
+    
 }
