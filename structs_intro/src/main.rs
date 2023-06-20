383f657e08bdef824199561aaa3065951dc697a9
milolynch <milobravman20@gmail.com>
2023-06-20 17:15:58 -0400

    first rust method implemented based on description and not looking at solution!

--- structs_intro/src/main.rs
+++ structs_intro/src/main.rs
@@ -1,45 +1,72 @@
 fn main() {
-
-
 // Most basic version
     // let width = 30;
     // let height = 50;
-
     // println!(
     //     "the area of the rectangle is {} pixels", area(width, height)
     // );
-
     // fn area (width: u32, height: u32) -> u32{
     //     width * height
     // }
 
 // Using tuples version
     // let rect = (30, 50);
-
     // println!("the area of the rectangle is {}", area(rect));
-
-
     // fn area (dimensions: (u32, u32)) -> u32{
     //     dimensions.0 * dimensions.1
     // }
 
 // Using structs version
-struct Rect {
-    width: i32,
-    height: i32
+    // let rec = Rect {
+    //     width: 30,
+    //     height: 50
+    // };
+    // if not borrowed here than the next print would not work
+    // println!("the area of the rectangle is {}", area(&rec));
+    // fn area (rect: &Rect) -> i32{
+        //     rect.height*rect.width
+        // }
+        // println!("the area of the rectangle is {}", area(&rec));
+
+// Using methods version
+    let rect1 = Rect{
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rect{
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rect{
+        width: 60,
+        height: 45,
+    };
+
+    println!("can rect1 hold rect2? {}", rect1.can_contain(&rect2));
+    println!("can rect1 hold rect2? {}", rect1.can_contain(&rect3));
+
+
 }
+    
+    struct Rect {
+        width: i32,
+        height: i32
+    }
 
-let rec = Rect {
-    width: 30,
-    height: 50
-};
+    impl Rect {
+        fn can_contain(&self, other: &Rect) -> bool{
+            if self.area() > other.area() 
+            {
+                true
+            }
+            else {
+                false
+            }
+        }
 
-// if not borrowed here than the next print would not work
-println!("the area of the rectangle is {}", area(&rec));
-
-fn area (rect: &Rect) -> i32{
-    rect.height*rect.width
-}
-println!("the area of the rectangle is {}", area(&rec));
-
-}
+        fn area(&self) -> i32 {
+            self.width*self.height
+        }
+    }
