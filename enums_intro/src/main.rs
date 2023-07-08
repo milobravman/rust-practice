f4e51ec99ad36f343da080a03d174e063e73b82e
milolynch <milobravman20@gmail.com>
2023-07-08 12:09:00 -0400

    testing enums

--- enums_intro/src/main.rs
+++ enums_intro/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+
+    enum Test{
+        First(i32),
+        Second(i32)
+    }
+
+    //let x = Test::First(30);
+    //println!("{}",x);
+    let y = 32;
+    println!("{}",y);
 }
