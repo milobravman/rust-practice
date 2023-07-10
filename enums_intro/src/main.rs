494aa7e4407907e6fad94e46e058c4b3b2780029
milolynch <milobravman20@gmail.com>
2023-07-09 20:16:33 -0400

    using match with enums
    I need to review 'Some'

--- enums_intro/src/main.rs
+++ enums_intro/src/main.rs
@@ -10,4 +10,14 @@ fn main() {
     //println!("{}",x);
     let y = 32;
     println!("{}",y);
+
+    fn online(x: Option<i32>) -> Option<i32>{
+        match x {
+            None => None,
+            Some(i) => Some(i+1)
+        }
+    }
+
+    let k = online(Some(5));
+    println!("{:?}", k)
 }
